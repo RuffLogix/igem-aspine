770ccda7ff9a695fefaa0da3df875c90c05b6150
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-09-03 13:40:09 +0700

    test: test_encode_sequence()

---
+++ tests/test_encode_sequence.py
@@ -0,0 +1,59 @@
+from utils import encode_sequence
+import constants
+import numpy as np
+
+ENCODE_SEQUENCE_TESTCASES = [
+    # Testcase 1. Single sequence.
+    (
+        ["ACGT"],
+        np.array(
+            [
+                [
+                    [1.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0],
+                ]
+            ],
+            dtype=np.float32,
+        ),
+        False,
+    ),
+    # Testcase 2. Empty sequence.
+    ([], constants.INVALID_SEQUENCE_EMPTY_ERROR, True),
+    # Testcase 3. Invalid nucleotides in sequence.
+    (["ACNT"], constants.INVALID_SEQUENCE_NUCLOTIDES_ERROR, True),
+    # Testcase 4. Multiple sequences.
+    (
+        ["ACCC", "GT"],
+        np.array(
+            [
+                [
+                    [1.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                ],
+                [
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0],
+                    [0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0],
+                ],
+            ],
+            dtype=np.float32,
+        ),
+        False,
+    ),
+]
+
+
+def test_encode_sequence():
+    for sequence_list, expected, is_error in ENCODE_SEQUENCE_TESTCASES:
+        if is_error:
+            try:
+                print(encode_sequence(sequence_list), expected)
+            except Exception as e:
+                assert str(e) == expected
+        else:
+            assert np.array_equal(encode_sequence(sequence_list), expected)
