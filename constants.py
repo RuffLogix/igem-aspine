c2a5459f1700d390f37688f47f366518fc0fb7db
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-09-03 12:37:40 +0700

    feature: encode_sequnce()

---
+++ constants.py
@@ -0,0 +1,4 @@
+SEQUENCE_NUCLOTIDES_ERROR = (
+    "Invalid nucleotide in sequence. Only 'A', 'C', 'G', 'T' are allowed."
+)
+SEQUENCE_INDEX = {"A": 0, "C": 1, "G": 2, "T": 3, "a": 0, "c": 1, "g": 2, "t": 3}
---
+++ utils.py
@@ -0,0 +1,63 @@
+from typing import List
+import numpy as np
+import numpy.typing as npt
+import constants
+
+
+def valid_sequence(sequence_list: List[str]) -> bool:
+    """
+    Check if a list of sequences is valid.
+
+    Parameters
+    ----------
+    sequence_list: List[str]
+        A list of sequences, where each sequence is a string of nucleotides (e.g. 'ACGT').
+
+    Returns
+    -------
+    bool
+        True if all sequences are valid, False otherwise.
+    """
+
+    for sequence in sequence_list:
+        for nucleotide in sequence:
+            if nucleotide not in constants.SEQUENCE_INDEX:
+                return False
+
+    return True
+
+
+def encode_sequence(sequence_list: List[str]) -> npt.NDArray[np.float32]:
+    """
+    Convert a list of sequences into a one-hot encoded tensor.
+
+    Parameters
+    ----------
+    sequences_list: List[str]
+        A list of sequences, where each sequence is a string of nucleotides (e.g. 'ACGT').
+
+    Returns
+    -------
+    np.ndarray
+        A 3D numpy array of shape (n_sequences, sequence_len, 4), where n_sequences is the number of sequences in the input list, and sequence_len is the length of each sequence. The last dimension has size 4 and corresponds to the one-hot encoding of the nucleotides (A, C, G, T).
+    """
+    if not valid_sequence(sequence_list):
+        raise ValueError()
+
+    # Get the number of sequences and the length of each sequence
+    n_sequences = len(sequence_list)
+    sequence_len = len(sequence_list[0])
+
+    # Initialize an array of zeros
+    sequence_vector = np.zeros((n_sequences, sequence_len, 4), dtype=np.float32)
+
+    # Fill in the array with the one-hot encoding
+    for i in range(n_sequences):
+        sequence = sequence_list[i]
+
+        # For each nucleotide in the sequence, set the corresponding one-hot encoding
+        for j in range(len(sequence_list[i])):
+            if sequence[j] in constants.SEQUENCE_INDEX:
+                sequence_vector[i, j, constants.SEQUENCE_INDEX[sequence[j]]] = 1.0
+
+    return sequence_vector
